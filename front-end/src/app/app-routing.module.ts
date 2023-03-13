18a57fd0d20f0b5de2f48c14ce38505ab54aa75a
Timothee <astagios01@gmail.com>
2023-03-13 10:54:48 +0100

    route v1

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListeQuizPage } from 'src/Components/ListeQuizPage/ListeQuizPage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/ListeQuizPage', component: ListeQuizPage },
+  { path: '', redirectTo: '/ListeQuizPage', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
