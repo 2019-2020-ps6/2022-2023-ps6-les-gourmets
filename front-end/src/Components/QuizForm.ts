eb6ee2565b2614b1dfcf62e874d39d089f473f6c
Timothee <astagios01@gmail.com>
2023-03-13 10:22:25 +0100

    rename

---
+++ front-end/src/Components/QuizForm.ts
@@ -0,0 +1,11 @@
+
+import { Component, OnInit } from '@angular/core';
+@Component({
+    selector: 'app-quiz-list',
+    templateUrl: './quiz-list.component.html',
+    styleUrls: ['./quiz-list.component.scss']
+})
+export class MyEpicComponent implements OnInit {
+    constructor() {}
+    ngOnInit(): void {}
+} 
