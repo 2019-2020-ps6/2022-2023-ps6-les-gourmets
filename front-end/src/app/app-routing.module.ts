6940184978fbd19e5e453a130badbf3d2ecdddb1
Timothee <astagios01@gmail.com>
2023-03-13 11:17:45 +0100

    Link ListeQuizPage

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { ListeQuizPage } from 'src/Components/ListeQuizPage/ListeQuizPage.component'; 
-=======
-import { ListeQuizPage } from 'src/Components/ListeQuizPage/ListeQuizPage.component';
->>>>>>> 18a57fd0d20f0b5de2f48c14ce38505ab54aa75a
 
 const routes: Routes = [
-  { path: '/ListeQuizPage', component: ListeQuizPage },
-  { path: '', redirectTo: '/ListeQuizPage', pathMatch: 'full' }
+  { path: 'ListeQuizPage', component: ListeQuizPage },
+  { path: '', redirectTo: 'ListeQuizPage', pathMatch: 'full' }
 ];
 
 @NgModule({
