8875d1e6c2765ebad69d4bdd4fb87272cbec71c1
Timothee <astagios01@gmail.com>
2023-03-18 18:55:40 +0100

    redirection ne marche plus deplus l import de quizService

--- front-end/src/Components/ListeQuiz/ListeQuiz.component.ts
+++ front-end/src/Components/ListeQuiz/ListeQuiz.component.ts
@@ -1,11 +1,21 @@
 
 import { Component, OnInit } from '@angular/core';
+import { QuizService } from 'src/service/service1.service';
+import { Router } from '@angular/router';
+import { Quiz } from 'src/models/quiz.model';
+
 @Component({
     selector: 'app-ListeQuiz',
     templateUrl: './ListeQuiz.component.html',
     styleUrls: ['./ListeQuiz.component.scss']
 })
 export class ListeQuiz implements OnInit {
-    constructor() {}
+    public quizList: Quiz[] = [];
+
+    constructor(private router: Router, public quizService: QuizService) {
+      this.quizService.quizzes$.subscribe((quizzes: Quiz[]) => {
+        this.quizList = quizzes;
+      });
+    }
     ngOnInit(): void {}
-} 
+}
--- front-end/src/mocks/QuizList.mocks.ts
+++ front-end/src/mocks/QuizList.mocks.ts
@@ -1,4 +1,20 @@
+import { Question } from "src/models/question.model";
 import { Quiz } from "src/models/quiz.model";
+
+export const QUESTION_ACTOR: Question = {
+  id: '1',
+  label: 'Jean Gabin a joué dans...',
+  answers: [
+      {
+          value: 'Les tuches II',
+          isCorrect: false,
+      },
+      {
+          value: 'La grande illusion',
+          isCorrect: true,
+      }
+  ]
+};
 
 
 export const QUIZ_LIST: Quiz[] = [
@@ -14,3 +30,4 @@ export const QUIZ_LIST: Quiz[] = [
     questions: [],
   }
 ];
+
