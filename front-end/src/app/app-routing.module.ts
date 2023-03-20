a1926bf60d25faf3b549c74fc9a0b7c8429a392c
Timothee <astagios01@gmail.com>
2023-03-20 08:49:24 +0100

    Big refractor on components position

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListeQuizPage } from 'src/Components/ListeQuizPage/ListeQuizPage.component';
-import { GamePageComponent } from 'src/Components/GamePage/GamePageComponent.component';
+import { ListeQuizPage } from './Components/Quizs/ListeQuizPage/ListeQuizPage.component';
+import { GamePageComponent } from './Components/Quizs/GamePage/GamePageComponent.component';
 
 const routes: Routes = [
   { path: 'ListeQuizPage', component: ListeQuizPage },
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListeQuizPage } from 'src/Components/ListeQuizPage/ListeQuizPage.component';
-import { ListeQuiz } from 'src/Components/ListeQuiz/ListeQuiz.component';
-import { QuizComponent } from 'src/Components/Quiz/Quiz.component';
-import { QuizFormComponent } from 'src/Components/QuizForm/QuizForm.component';
+import { ListeQuizPage } from './Components/Quizs/ListeQuizPage/ListeQuizPage.component';
+import { ListeQuiz } from './Components/Quizs/ListeQuiz/ListeQuiz.component';
+import { QuizComponent } from './Components/Quizs/Quiz/Quiz.component';
+import { QuizFormComponent } from './Components/Quizs/QuizForm/QuizForm.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { GamePageComponent } from 'src/Components/GamePage/GamePageComponent.component';
+import { GamePageComponent } from './Components/Quizs/GamePage/GamePageComponent.component';
 
 @NgModule({
   declarations: [
