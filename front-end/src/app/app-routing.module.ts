d5ff55ca47b46e317677fd76e8c8dc846526a2b2
Yashinon <yael.bubu@gmail.com>
2023-03-20 11:05:08 +0100

    rebase pour Dev 2 #9

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListeQuizPage } from 'src/Components/Quizs/ListeQuizPage/ListeQuizPage.component';
-import { GamePageComponent } from 'src/Components/Quizs/GamePage/GamePageComponent.component';
+import { ListeQuizPage } from 'src/app/Components/Quizs/ListeQuizPage/ListeQuizPage.component';
+import { GamePageComponent } from 'src/app/Components/Quizs/GamePage/GamePageComponent.component';
 
 const routes: Routes = [
   { path: 'ListeQuizPage', component: ListeQuizPage },
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListeQuizPage } from 'src/Components/Quizs/ListeQuizPage/ListeQuizPage.component';
-import { ListeQuiz } from 'src/Components/Quizs/ListeQuiz/ListeQuiz.component';
-import { QuizComponent } from 'src/Components/Quizs/Quiz/Quiz.component';
-import { QuizFormComponent } from 'src/Components/Quizs/QuizForm/QuizForm.component';
+import { ListeQuizPage } from 'src/app/Components/Quizs/ListeQuizPage/ListeQuizPage.component';
+import { ListeQuiz } from 'src/app/Components/Quizs/ListeQuiz/ListeQuiz.component';
+import { QuizComponent } from 'src/app/Components/Quizs/Quiz/Quiz.component';
+import { QuizFormComponent } from 'src/app/Components/Quizs/QuizForm/QuizForm.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { GamePageComponent } from 'src/Components/Quizs/GamePage/GamePageComponent.component';
-import { QuestionComponent } from 'src/Components/Questions/Question/Question.component';
-import { ListeQuestion } from 'src/Components/Questions/ListeQuestion/ListeQuestion.component';
-import { QuestionFormComponent } from 'src/Components/Questions/QuestionForm/QuestionForm.component';
+import { GamePageComponent } from 'src/app/Components/Quizs/GamePage/GamePageComponent.component';
+import { QuestionComponent } from 'src/app/Components/Questions/Question/Question.component';
+import { ListeQuestion } from 'src/app/Components/Questions/ListeQuestion/ListeQuestion.component';
+import { QuestionFormComponent } from 'src/app/Components/Questions/QuestionForm/QuestionForm.component';
 
 
 @NgModule({
