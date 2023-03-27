6a467b423bc6f84cd025b1ffc42b92859ded2b92
Timothee <astagios01@gmail.com>
2023-03-27 11:51:19 +0200

    suppr gamePage

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ListeQuizPage } from './Components/Quizs/ListeQuizPage/ListeQuizPage.component';
 import { ListeUserPage } from './Components/Users/UserListPage/ListeUserPage.component';
-import { GamePageComponent } from './Components/Quizs/GamePage/GamePageComponent.component';
 import { MenuComponent } from './Components/Menu/MenuComponents/Menu.component';
 import { MenuChoixCreation } from './Components/Menu/MenuChoixCreationComponent/MenuChoixCreation.component';
 import { QuizFormComponent } from './Components/Quizs/QuizForm/QuizForm.component';
@@ -18,8 +17,6 @@ const routes: Routes = [
   { path: '', redirectTo: '/Menu', pathMatch: 'full' },
   { path: 'Menu', component: MenuComponent },
   { path: 'MenuChoixCreation', component:MenuChoixCreation},
-
-  { path: 'GamePageComponent', component: GamePageComponent },
 
   { path: 'ListeUserPage', component: ListeUserPage },
   { path: 'UserForm', component:UserForm},
