d3e329dc6206444f7618481e4f86215aa6a455ec
Timothee <astagios01@gmail.com>
2023-03-29 18:11:45 +0200

    user annuler changements #6

--- front-end/src/app/Components/Users/UserProfilePage/UserProfilePage.component.ts
+++ front-end/src/app/Components/Users/UserProfilePage/UserProfilePage.component.ts
@@ -45,6 +45,9 @@ export class UserProfilePage implements OnInit {
     }
     cancelChanges() : void {
         this.UserModified = this.User;
+        this.modifs['controls']['name'].setValue(this.User.name);
+        this.modifs['controls']['surname'].setValue(this.User.surname);
+        this.modifs['controls']['aggressivness'].setValue(this.User.aggressivness);
     }
 
 
