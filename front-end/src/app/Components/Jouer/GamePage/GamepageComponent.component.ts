7df80ff397c9d18bbd9b405aac5170dbad228ec0
Timothee <astagios01@gmail.com>
2023-04-01 14:59:53 +0200

    parse user gamepage

--- front-end/src/app/Components/Jouer/GamePage/GamepageComponent.component.ts
+++ front-end/src/app/Components/Jouer/GamePage/GamepageComponent.component.ts
@@ -11,115 +11,115 @@ import { Router } from '@angular/router';
 
 
 @Component({
-    selector: 'app-GamePage',
-    templateUrl: './GamePageComponent.component.html',
-    styleUrls: ['./GamePageComponent.component.scss']
-  })
+  selector: 'app-GamePage',
+  templateUrl: './GamePageComponent.component.html',
+  styleUrls: ['./GamePageComponent.component.scss']
+})
 
-  export class GamePageComponent implements OnInit {
-    user!: User;
-    quiz!: Quiz;
-    currentQuestionIndex = 0;
-    answers:boolean[]
-    currentQuestion: Question;
-    validate: boolean;
-    end: boolean;
-    nbAnswers: number;
+export class GamePageComponent implements OnInit {
+  user!: User;
+  quiz!: Quiz;
+  currentQuestionIndex = 0;
+  answers: boolean[]
+  currentQuestion: Question;
+  validate: boolean;
+  end: boolean;
+  nbAnswers: number;
 
-    @HostListener("document:mousedown",['$event'])
-    onClick(event: MouseEvent){this.userService.mouseClickInQuiz(event);}
+  @HostListener("document:mousedown", ['$event'])
+  onClick(event: MouseEvent) { this.userService.mouseClickInQuiz(event); }
 
-    @Output()
-   /* currentQuestion: EventEmitter<Question>=new EventEmitter<Question>();
+  @Output()
+  /* currentQuestion: EventEmitter<Question>=new EventEmitter<Question>();
 
-    changeQuestion() : void{
-        this.currentQuestion.emit(this.quiz.nextQuestion());
-    }*/
+   changeQuestion() : void{
+       this.currentQuestion.emit(this.quiz.nextQuestion());
+   }*/
 
 
-    selectedAnswers = {};
+  selectedAnswers = {};
 
-    submit(): void {
-      console.log(this.selectedAnswers);
-    }
-
-    constructor(private router: Router,public quizService:QuizService, public userService:UserService) {
-      //this.currentQuestion = this.quiz.questions[0];
-      this.userService.UserSelected$.subscribe((user: User) => {
-        this.user = user;
-      });
-      this.quizService.quizSelected$.subscribe((quiz: Quiz) => {
-        this.quiz = quiz;
-      });
-      // Randomise question order
-      this.quiz.questions.sort(() => {
-        return Math.random() - 0.5;
-      });
-      this.currentQuestion = this.quiz.questions[0];
-      this.validate = false;
-      this.answers = [];
-      this.end = false;
-      this.nbAnswers = this.quiz.questions.length;
-    }
-
-    ngOnInit(): void {
-      //this.currentQuestion = this.quiz.questions[this.currentQuestionIndex];
-      this.currentQuestion = this.quiz.questions[0];
-    }
-
-    onNextQuestion(): void {
-      console.log(this.answers);
-      if (!this.validate || !this.answers[this.currentQuestionIndex]){
-        this.quiz.questions.push(this.currentQuestion);
-      }
-      this.currentQuestionIndex++;
-      if (this.currentQuestionIndex < this.quiz.questions.length) {
-        this.currentQuestion = this.quiz.questions[this.currentQuestionIndex];
-        this.validate = false;
-      }
-      else{ this.endQuiz()}
-    }
-
-    validateQuestion(answer:boolean): void {
-      this.validate = true;
-      this.answers[this.currentQuestionIndex] = answer;
-      let i:number = 0;
-      for(let a of this.answers){
-        if(a){
-          i++;
-        }
-      }
-      if (i>=this.nbAnswers) {
-        this.end = true;
-        if(!this.user.answerDisplay){
-          this.endQuiz();
-        }
-      }
-      if(!this.user.answerDisplay){
-        this.onNextQuestion();
-      }
-    }
-
-    onPreviousQuestion(): void {
-      this.currentQuestionIndex--;
-      if (this.currentQuestionIndex >= 0) {
-        this.currentQuestion = this.quiz.questions[this.currentQuestionIndex];
-      }
-      else{ this.quitQuiz();}
-    }
-
-    endQuiz():void{
-      this.router.navigate(['/EndPageComponent']);
-    }
-
-    quitQuiz():void{
-      this.currentQuestionIndex++;
-    }
-
-    ChangeMusic(event : any){
-      this.userService.fadeVolume(event.target.checked);
-    }
-
+  submit(): void {
+    console.log(this.selectedAnswers);
   }
+
+  constructor(private router: Router, public quizService: QuizService, public userService: UserService) {
+    //this.currentQuestion = this.quiz.questions[0];
+    this.userService.UserSelected$.subscribe((user: User) => {
+      this.user = JSON.parse(JSON.stringify(user));
+    });
+    this.quizService.quizSelected$.subscribe((quiz: Quiz) => {
+      this.quiz = JSON.parse(JSON.stringify(quiz));
+    });
+    // Randomise question order
+    this.quiz.questions.sort(() => {
+      return Math.random() - 0.5;
+    });
+    this.currentQuestion = this.quiz.questions[0];
+    this.validate = false;
+    this.answers = [];
+    this.end = false;
+    this.nbAnswers = this.quiz.questions.length;
+  }
+
+  ngOnInit(): void {
+    //this.currentQuestion = this.quiz.questions[this.currentQuestionIndex];
+    //this.currentQuestion = this.quiz.questions[0];
+  }
+
+  onNextQuestion(): void {
+    console.log(this.answers);
+    if (!this.validate || !this.answers[this.currentQuestionIndex]) {
+      this.quiz.questions.push(this.currentQuestion);
+    }
+    this.currentQuestionIndex++;
+    if (this.currentQuestionIndex < this.quiz.questions.length) {
+      this.currentQuestion = this.quiz.questions[this.currentQuestionIndex];
+      this.validate = false;
+    }
+    else { this.endQuiz() }
+  }
+
+  validateQuestion(answer: boolean): void {
+    this.validate = true;
+    this.answers[this.currentQuestionIndex] = answer;
+    let i: number = 0;
+    for (let a of this.answers) {
+      if (a) {
+        i++;
+      }
+    }
+    if (i >= this.nbAnswers) {
+      this.end = true;
+      if (!this.user.answerDisplay) {
+        this.endQuiz();
+      }
+    }
+    if (!this.user.answerDisplay) {
+      this.onNextQuestion();
+    }
+  }
+
+  onPreviousQuestion(): void {
+    this.currentQuestionIndex--;
+    if (this.currentQuestionIndex >= 0) {
+      this.currentQuestion = this.quiz.questions[this.currentQuestionIndex];
+    }
+    else { this.quitQuiz(); }
+  }
+
+  endQuiz(): void {
+    this.router.navigate(['/EndPageComponent']);
+  }
+
+  quitQuiz(): void {
+    this.currentQuestionIndex++;
+  }
+
+  ChangeMusic(event: any) {
+    this.userService.fadeVolume(event.target.checked);
+  }
+
+}
 
 
