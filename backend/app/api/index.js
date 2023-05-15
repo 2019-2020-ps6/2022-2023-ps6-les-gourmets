330b59210cc5ba6b44b25032333a769f95bcc907
krystodagues <90374965+krystodagues@users.noreply.github.com>
2023-05-15 11:19:23 +0200

    users #42

--- backend/app/api/index.js
+++ backend/app/api/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 const quizRouter = require('./quiz')
+const userRouter = require('./users')
 
 const router = new Router()
 router.get('/status', (req, res) => res.status(200).json('ok'))
 router.use('/quiz', quizRouter)
+router.use('/users', userRouter)
 
 
 module.exports = router
---
+++ backend/app/api/users/index.js
@@ -0,0 +1,27 @@
+const { Router } = require('express')
+
+const { Users, User } = require('../../models')
+
+const router = new Router();
+
+router.get('/', (req, res) => {
+    try {
+        res.status(200).json(User.get())
+    } 
+    catch (err) {
+            console.log(err)
+            res.status(500).json({message: 'Something went wrong', err})  
+            }
+         } )
+router.post('/', (req, res) => {
+    try {
+        const quiz = User.create(req.body)
+        res.status(201).json(Users)
+    } 
+    catch (err) {
+            console.log(err)
+            res.status(500).json({message: 'Something went wrong', err})  
+            }
+         } )
+
+module.exports = router
