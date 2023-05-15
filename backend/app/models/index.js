51c4e8941babf2358c98d5bf18a5ccce1eb6425f
Timothee <astagios01@gmail.com>
2023-05-15 09:55:08 +0200

    all models #43

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,5 +1,11 @@
 // const YourModel = require('./your-model.model.js')
+const User = require('./User.model.js')
+const Quiz = require('./quiz.model.js')
+const Question = require('./question.model.js')
 
 module.exports = {
   // YourModel,
+  User,
+  Quiz,
+  Question,
 }
