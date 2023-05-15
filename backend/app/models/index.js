53bcfab660d161c45de57ad4b00202d9047f3486
Timothee <astagios01@gmail.com>
2023-05-15 11:05:26 +0200

    models prof v1 #43

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -2,10 +2,12 @@
 const User = require('./User.model.js')
 const Quiz = require('./quiz.model.js')
 const Question = require('./question.model.js')
+const Answer = require('./answer.model.js')
 
 module.exports = {
   // YourModel,
   User,
   Quiz,
   Question,
+  Answer
 }
--- backend/app/models/question.model.js
+++ backend/app/models/question.model.js
@@ -1,18 +1,13 @@
 const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
+const { AnswerSchema } = require('./answer.model.js')
 
 module.exports = new BaseModel('Question', {
   type: Joi.string(),
   images: Joi.array(),
   label: Joi.string().required(),
-  answers: Joi.array().required(),
+  answers: Joi.array().items(AnswerSchema).required(),
   trueAnswer: Joi.number().required(),
   falseAnswer: Joi.number().required(),
   estFacile: Joi.boolean().required(),
 })
-
-module.exports = new BaseModel('Answer', {
-    type: Joi.string(),
-    value: Joi.string().required(),
-    isCorrect: Joi.boolean().required(),
-    })
