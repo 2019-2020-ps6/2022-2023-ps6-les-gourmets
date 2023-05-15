e0b59f7adeb1ad97bd29b0010dec4978b6bda690
Timothee <astagios01@gmail.com>
2023-05-15 11:06:54 +0200

    models prof v1.5 #43

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -2,7 +2,7 @@
 const User = require('./User.model.js')
 const Quiz = require('./quiz.model.js')
 const Question = require('./question.model.js')
-const Answer = require('./answer.model.js')
+const { Answer } = require('./answer.model.js')
 
 module.exports = {
   // YourModel,
