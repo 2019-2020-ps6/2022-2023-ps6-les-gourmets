df1da3d92cbdcf4412d85b21eff71f022af2bedd
Timothee <astagios01@gmail.com>
2023-05-15 09:55:19 +0200

    all models 2 #43

---
+++ backend/app/models/User.model.js
@@ -0,0 +1,13 @@
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model.js')
+
+module.exports = new BaseModel('User', {
+    name: Joi.string().required(),
+    surname : Joi.string().required(),
+    aggressivness : Joi.number().required(),
+    answerDisplay: Joi.boolean().required(),
+    quizzes : Joi.array().required(),
+    music : Joi.array().required(),
+    id : Joi.number().required(),
+    passivity : Joi.number().required(),
+})
---
+++ backend/app/models/quiz.model.js
@@ -0,0 +1,11 @@
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model.js')
+
+module.exports = new BaseModel('Quiz', {
+    id: Joi.number().required(),
+    name: Joi.string().required(),
+    questions: Joi.array().required(),
+    easyQuestions: Joi.array().required(),
+    timerMoyen: Joi.number().required(),
+    times: Joi.array().required(),
+    })
