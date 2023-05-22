f6aa0a39a42bbd1635bf3fa6d9f1d0b0c6f7f386
krystodagues <90374965+krystodagues@users.noreply.github.com>
2023-05-22 08:40:05 +0200

    les questions back #42

--- backend/app/api/index.js
+++ backend/app/api/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
 const quizRouter = require('./quiz')
 const userRouter = require('./users')
+const questionRouter = require('./questions')
 
 const router = new Router()
 router.get('/status', (req, res) => res.status(200).json('ok'))
 router.use('/quiz', quizRouter)
 router.use('/users', userRouter)
+router.use('/questions', questionRouter)
 
 
 module.exports = router
