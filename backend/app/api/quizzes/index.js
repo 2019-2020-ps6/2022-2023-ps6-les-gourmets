53d3eb946a9902f3d88b1e8c5c901035ee194ffa
krystodagues <90374965+krystodagues@users.noreply.github.com>
2023-05-22 09:33:27 +0200

    vraie deletion #42

--- backend/app/api/quizzes/index.js
+++ backend/app/api/quizzes/index.js
@@ -23,7 +23,17 @@ router.post('/', (req, res) => {
             res.status(500).json({message: 'Something went wrong', err})  
             }
          } )
-
+router.delete('/', (req, res) => {
+    try {
+        const id = req.body.id
+        Quiz.delete(id)
+        res.status(201).json(Quiz)
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({message: 'Something went wrong', err})
+    }
+})
 
 
 module.exports = router
