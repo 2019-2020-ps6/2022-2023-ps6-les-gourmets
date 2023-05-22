8bd72fe15ca582143ae833b6cc365cd15ef213c5
Timothee <astagios01@gmail.com>
2023-05-22 10:47:51 +0200

    user update #42

--- backend/app/api/users/index.js
+++ backend/app/api/users/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 
-const { Users, User } = require('../../models')
+const { User } = require('../../models')
 
 const router = new Router();
 
@@ -13,15 +13,49 @@ router.get('/', (req, res) => {
             res.status(500).json({message: 'Something went wrong', err})  
             }
          } )
+
+router.get('/:userId', (req, res) => {
+    try {
+        res.status(200).json(User.getById(req.params.answerId))
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({message: 'Something went wrong', err})
+    }
+    }   )
+
+
 router.post('/', (req, res) => {
     try {
-        const quiz = User.create(req.body)
-        res.status(201).json(Users)
+        const user = User.create(req.body)
+        res.status(201).json(user)
     } 
     catch (err) {
             console.log(err)
             res.status(500).json({message: 'Something went wrong', err})  
             }
          } )
+
+router.delete('/:userId', (req, res) => {
+    try {
+        User.delete(req.params.userId)
+        res.status(201).end()
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({message: 'Something went wrong', err})
+    }
+})
+
+router.patch('/:userId', (req, res) => {
+    try {
+        const user = User.update(req.params.userId, req.body)
+        res.status(201).json(user)
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({message: 'Something went wrong', err})
+    }
+})
 
 module.exports = router
