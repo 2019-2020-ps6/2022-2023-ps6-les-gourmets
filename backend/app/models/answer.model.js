b5bc1925f75ab10f1702740828749884acbf8a98
krystodagues <90374965+krystodagues@users.noreply.github.com>
2023-06-05 08:56:51 +0200

    backend question sécurité #46

--- backend/app/models/answer.model.js
+++ backend/app/models/answer.model.js
@@ -1,12 +1,20 @@
 const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
 
+
 const Answer = new BaseModel('Answer', {
     type: Joi.string(),
     value: Joi.string().required(),
     isCorrect: Joi.boolean().required(),
     })
 
+const AnswerSchema = {
+    type: Joi.string(),
+    value: Joi.string().required(),
+    isCorrect: Joi.boolean().required(),
+    }
+
+
 module.exports = {
-    Answer
+    Answer, AnswerSchema
 }
--- backend/app/models/question.model.js
+++ backend/app/models/question.model.js
@@ -1,13 +1,14 @@
 const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
 const number = require('joi/lib/types/number/index.js')
+const {AnswerSchema} = require('./answer.model.js')
 
 module.exports = new BaseModel('Question', {
   type: Joi.string(),
-  theme: Joi.string(),
+  themes: Joi.array(),
   images: Joi.array(),
   label: Joi.string().required(),
-  answers: Joi.array().items(number).required(),
+  answers: Joi.array().items(AnswerSchema).required(),
   trueAnswer: Joi.number().required(),
   falseAnswer: Joi.number().required(),
   estFacile: Joi.boolean().required(),
