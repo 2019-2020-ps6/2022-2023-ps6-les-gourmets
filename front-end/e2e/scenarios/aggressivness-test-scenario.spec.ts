dd97bec042fc3da940d921a4ea4e6fb7a3f17552
Timothee <astagios01@gmail.com>
2023-06-13 09:33:18 +0200

    add default user config #52

--- front-end/e2e/scenarios/aggressivness-test-scenario.spec.ts
+++ front-end/e2e/scenarios/aggressivness-test-scenario.spec.ts
@@ -4,6 +4,8 @@ import { testUrl } from 'e2e/e2e.config';
 test.describe('Tests de partie', () => {
   test('should display the popup', async ({ page }) => {
     await page.goto(testUrl);
+    await page.getByRole('button', { name: 'Jouer' }).click();
+    await page.locator('app-user').filter({ hasText: 'Nom : Arnaud Prénom : DumanoisSélectionner' }).getByRole('button', { name: 'Sélectionner' }).click();
 
   });
 });
