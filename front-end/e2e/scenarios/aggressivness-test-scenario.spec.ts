469e69b745ee009cdd710acb6268df477fca1428
krystodagues <90374965+krystodagues@users.noreply.github.com>
2023-06-16 10:53:28 +0200

    test différence taux d'aggressivité #56

--- front-end/e2e/scenarios/aggressivness-test-scenario.spec.ts
+++ front-end/e2e/scenarios/aggressivness-test-scenario.spec.ts
@@ -2,36 +2,25 @@ import {test, expect, selectors} from '@playwright/test';
 import { testUrl } from 'e2e/e2e.config';
 
 test.describe('Tests de partie', () => {
-  test('should display the popup and them play the easy questions', async ({ page }) => {
+  test('should display the popup and then play the easy questions', async ({ page }) => {
     await page.goto(testUrl);
     await page.getByRole('button', { name: 'Jouer' }).click();
     await page.locator('app-user').filter({ hasText: 'ArnaudDumanois' }).getByRole('button', { name: 'Sélectionner' }).click();
     await page.locator('app-quiz').filter({ hasText: 'Quiz de Arnaud' }).getByRole('button', { name: 'Choisir' }).click();
-    let continueQuizVisible = false;
 
-   /* async function playQuestions(page: any) {
-      const continueQuizVisible = await page.isVisible('Voulez-vous continuer le Quiz');
-      if (continueQuizVisible) {
-        return;
-      } else {
-        await page.getByRole('button', { name: 'Passer la question' }).click();
-        await playQuestions(page);
-      }
-    }*/
+
   
     for(let i = 0; i < 12; i++){
       await page.getByRole('button', { name: 'Passer la question' }).click();
+      await page.waitForTimeout(2500);
     }
+    await expect(page.getByText('Voulez-vous continuer le Quiz')).not.toBeVisible();//la popup a du ne pas s'afficher
 
-    /*
-    while (!continueQuizVisible) {
-      continueQuizVisible = await page.isVisible('Voulez-vous continuer le Quiz');
-      if(!continueQuizVisible){
-        await page.getByRole('button', { name: 'Passer la question' }).click();
-      }
-      continueQuizVisible = await page.isVisible('Voulez-vous continuer le Quiz');
-    }*/
 
+    await page.waitForTimeout(6000);//on reset l'agressivité
+    for(let i = 0; i < 12; i++){
+      await page.getByRole('button', { name: 'Passer la question' }).click();
+    }
 
     await expect(page.getByText('Voulez-vous continuer le Quiz')).toBeVisible();//la popup a du s'afficher
     await page.getByRole('button',{name:'Continuer'}).click();
@@ -46,10 +35,31 @@ test.describe('Tests de partie', () => {
     await page.getByRole('button',{name:'Arrêter'}).click();
     await expect(page.getByText('Liste Quiz')).toBeVisible();//on est revenu sur la page des quiz
 
+  });
+
+  test('should wait more display popup', async ({ page }) => {//on teste la popup avec un profil moins aggressif
+    await page.goto(testUrl);
+    await page.getByRole('button', { name: 'Jouer' }).click();
+    await page.locator('app-user').filter({ hasText: 'TimothéeJuillet' }).getByRole('button', { name: 'Sélectionner' }).click();
+    await page.locator('app-quiz').filter({ hasText: 'Quiz de Arnaud' }).getByRole('button', { name: 'Choisir' }).click();
+
+
   
-  
+    for(let i = 0; i < 12; i++){
+      await page.getByRole('button', { name: 'Passer la question' }).click();
+    }
+
+    await expect(page.getByText('Voulez-vous continuer le Quiz')).toBeVisible();//la popup a du s'afficher parce que agressivité différente
+    await page.getByRole('button',{name:'Continuer'}).click();
+
+    await page.waitForTimeout(17000);
+    await expect(page.getByText('Voulez-vous continuer le Quiz')).not.toBeVisible();//la popup a du s'afficher
 
   });
+
+
+
+
 
 
 });
